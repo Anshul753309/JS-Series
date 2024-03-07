6a650bcdc9716390b2e2bcdc757512529467650d
Anshul753309 <143771452+Anshul753309@users.noreply.github.com>
2024-03-07 14:25:56 +0000

    Conversion Data Types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = "33";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+let isLoggedIn = 2;
+
+let booleanIsLoggeDIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggeDIn);
+
+let num = 33;
+
+let stringNumber = String(num);
+console.log(stringNumber);
+console.log(typeof stringNumber);
