95f0aa4210630f1341989a5a5e0401d1fd06fa5c
Anshul753309 <143771452+Anshul753309@users.noreply.github.com>
2024-03-07 13:51:38 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "ash@google.com";
+var accountPassword = "12345";
+accountCity = "Surrey";
+
+//accountId = 2
+accountEmail = "ash@ash.ca"
+accountPassword = "23498"
+accountCity = "Vancouver"
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //newer version of js
+
+let name = 'Anshul'; //string data type
+let age = 18
+let isLoggedIn = false;// boolean type
+
+// number => 2 to power 53
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined
+// symbol
+
+//object 
+console.log(typeof "Anshul");
